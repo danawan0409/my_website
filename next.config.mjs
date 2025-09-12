f9d4da96ed64cc3b4234fd504337dcd0d39b87b0
danawan0409 <122662430+danawan0409@users.noreply.github.com>
2025-09-12 01:17:25 -0400

    killing myself

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  basePath: '/repo-name',
+  assetPrefix: '/repo-name/',
 };
 
 export default nextConfig;
