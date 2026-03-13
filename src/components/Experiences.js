32c8f7716ecaa85f877fa86d6f5d485810576ab1
danawan0409 <122662430+danawan0409@users.noreply.github.com>
2026-03-13 01:23:45 -0400

    change experiences images

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -17,7 +17,13 @@ export default function Experiences() {
             objectFit="contain"
           />
           <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Telus_Health_logo.svg/2560px-Telus_Health_logo.svg.png"
+            src="https://images.ctfassets.net/fltupc9ltp8m/2gpVnhLZ33jCLjQDQBJL4T/1a52c0d7010694ad4b207f31e9c4db19/OG_Image-TELUS_Health_Logo-EN.png"
+            width="100%"
+            height="auto"
+            objectFit="contain"
+          />
+          <Image
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clio.com%2Fca%2Fwp-content%2Fuploads%2Fsites%2F6%2F2025%2F07%2FClio_Meta-Image_General.png&f=1&nofb=1&ipt=1277f7e3dbba43fb579b5fce7856c34d44c05cdb27164412670185e9c2dc1dcf"
             width="100%"
             height="auto"
             objectFit="contain"
